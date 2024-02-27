ad9fadcc39815f32795517ded22003796287efed
Fernando González <149832877+FerGonzalezDEV@users.noreply.github.com>
2024-02-27 16:26:25 -0500

    Add files via upload

---
+++ guess-the-number/app.js
@@ -0,0 +1,42 @@
+const number = document.getElementById("number")
+const numberBtns = document.querySelectorAll(".number-button")
+const resetBtn = document.getElementById("reset-button")
+const mainText = document.getElementById("main-text")
+
+let randomNumber = Math.floor(Math.random() * 10) + 1;
+
+Array.from(numberBtns).map(numberBtn =>{
+    numberBtn.addEventListener( "click", ()=> {
+        number.innerHTML = numberBtn.innerHTML;
+        numberBtn.disabled = true;
+        if (number.innerHTML == randomNumber){
+            number.style.color = "green";
+            numberBtn.style.color = "white"
+            numberBtn.style.background = "darkorchid"
+            endGame();
+        } else {
+            number.style.color = "red"
+        }
+    })
+})
+
+function endGame(){
+    Array.from(numberBtns).map(numberBtn =>{
+        numberBtn.disabled = true;
+    })
+    resetBtn.style.display = "block"
+    mainText.innerHTML = "You guessed the number!!";
+    mainText.style.color = "green";
+}
+
+function resetGame(){
+    mainText.innerHTML = "Guess a number between <span class='purple'>1</span> and <span class='purple'>10</span>";
+    mainText.style.color = "black";
+    number.innerHTML = "";
+    Array.from(numberBtns).map(numberBtn =>{
+        numberBtn.disabled = false;
+        numberBtn.style.color = "";
+        numberBtn.style.background = "";
+    })
+    randomNumber = Math.floor(Math.random()*10)+1;
+}
